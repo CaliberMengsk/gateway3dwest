1b7264ffbfdc0723388ab1cbef5921b076363213
Randal James Phillips <caliber_mengsk@yahoo.com>
2015-08-31 23:57:55 -0500

    Removed Rain ai thing to get my branch working Beside that, don't know what the other changes are for. It seems the gitignore is slightly out of date so I updated that. I think the rest of the files are from not seeing that change yet. Started a basic hovering enemy ai. Just some setup variables.

---
+++ Assets/Scripts/Enemy AI/HoveringBadguy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HoveringBadguy : MonoBehaviour {
+
+    public float wantedHeight = 5f;
+    public float wantedDistance = 15f;
+
+    public float speed = 3f;
+    public float rotationalSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
